a1787c605e3238c03eb6f43f1e8230a337af9930
Yuly Paola Hernández Ordoñez <61712988+paolahernandezor@users.noreply.github.com>
2022-10-25 19:55:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //console.log('hola')
 window.onload=()=>{
-    const parrafo = document.getElementById('text')
+    /*const parrafo = document.getElementById('text')
     //console.log(parrafo.innerText)
     //parrafo.innerText='Nuevo Parrafo'
-    parrafo.innerHTML='<li>Elemento 1</li><li>Elemeto 2</li>'
+    parrafo.innerHTML='<li>Elemento 1</li><li>Elemeto 2</li>'*/
+    const form= document.getElementById('todo-from');
+    form.onsubmit=(e)=>{
+
+    }
 }
-  
