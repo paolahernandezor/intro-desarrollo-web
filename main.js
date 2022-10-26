ddcab4ae8bce98f7f5627482b85e848a3575910c
Yuly Paola Hernández Ordoñez <61712988+paolahernandezor@users.noreply.github.com>
2022-10-25 19:41:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//console.log('hola')
+window.onload=()=>{
+    const parrafo = document.getElementById('text')
+    //console.log(parrafo.innerText)
+    //parrafo.innerText='Nuevo Parrafo'
+    parrafo.innerHTML='<li>Elemento 1</li><li>Elemeto 2</li>'
+}
+  
